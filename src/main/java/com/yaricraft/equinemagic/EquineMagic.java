35755a9c2507cbe32299f2751978ee60d2376ffa
Timothy Fike <tafike@gmail.com>
2014-09-17 13:05:48 -0400

    Finish recipes

--- src/main/java/com/yaricraft/equinemagic/EquineMagic.java
+++ src/main/java/com/yaricraft/equinemagic/EquineMagic.java
@@ -17,6 +17,8 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.BlockStone;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.WeightedRandomChestContent;
 import net.minecraftforge.common.ChestGenHooks;
@@ -39,19 +41,10 @@ public class EquineMagic
         EquineMagicBlock.init();
         EquineMagicFluid.init();
 
-        OreDictionary.registerOre(ModNames.ORE_CHROMA, EquineMagicBlock.blockOreChroma);
-        OreDictionary.registerOre(ModNames.ORE_PEGAGIN, EquineMagicBlock.blockOrePegagin);
-        OreDictionary.registerOre(ModNames.ORE_SPECTRA, EquineMagicBlock.blockOreSpectra);
-
-        OreDictionary.registerOre(ModNames.DUST_CHROMA, EquineMagicItem.dustChroma);
-        OreDictionary.registerOre(ModNames.DUST_SPECTRA, EquineMagicItem.dustSpectra);
-
-        OreDictionary.registerOre(ModNames.DUST_SPECTRA, EquineMagicItem.dustSpectra);
-
-        OreDictionary.registerOre(ModNames.BLOCK_DECOR, EquineMagicBlock.blockDecor);
+        registerDictionaryItems();
 	}
 
-	@Mod.EventHandler
+    @Mod.EventHandler
 	public void init(FMLInitializationEvent event)
 	{
         // Register the Items Event Handler
@@ -63,7 +56,43 @@ public class EquineMagic
         ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_LIBRARY, new WeightedRandomChestContent(EquineMagicItem.dustAlicorn, 0, 1, 2, 5));
         ChestGenHooks.addItem(ChestGenHooks.PYRAMID_JUNGLE_CHEST, new WeightedRandomChestContent(EquineMagicItem.dustAlicorn, 0, 1, 2, 5));
 
-        // Add Recipes
+        addRecipes();
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+        // NOOP
+    }
+
+    private void registerDictionaryItems()
+    {
+        OreDictionary.registerOre(ModNames.ORE_CHROMA, EquineMagicBlock.blockOreChroma);
+        OreDictionary.registerOre(ModNames.ORE_PEGAGIN, EquineMagicBlock.blockOrePegagin);
+        OreDictionary.registerOre(ModNames.ORE_SPECTRA, EquineMagicBlock.blockOreSpectra);
+
+        OreDictionary.registerOre(ModNames.DUST_CHROMA, EquineMagicItem.dustChroma);
+        OreDictionary.registerOre(ModNames.DUST_PEGAGIN, EquineMagicItem.dustPegagin);
+        OreDictionary.registerOre(ModNames.DUST_SPECTRA, EquineMagicItem.dustSpectra);
+        OreDictionary.registerOre(ModNames.DUST_SILKY, EquineMagicItem.dustSilky);
+        OreDictionary.registerOre(ModNames.DUST_SILKY_GUNPOWDER, EquineMagicItem.getDustSilkyGunpowder);
+
+        OreDictionary.registerOre(ModNames.BLOCK_DECOR, EquineMagicBlock.blockDecor);
+        OreDictionary.registerOre(ModNames.BLOCK_SILKY_TNT, EquineMagicBlock.blockSilkyTNT);
+        OreDictionary.registerOre(ModNames.BLOCK_SOLAR_CAULDRON, EquineMagicBlock.solarCauldron);
+
+        if(OreDictionary.getOres("blockWool").size() < 16)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                OreDictionary.registerOre("blockWool", new ItemStack(Blocks.wool, 1, i));
+            }
+        }
+    }
+
+    private void addRecipes()
+    {
+        //
         for ( int i = 15; i >= 0; i--)
         {
             GameRegistry.addShapelessRecipe(
@@ -72,6 +101,25 @@ public class EquineMagic
             );
         }
 
+
+            GameRegistry.addShapelessRecipe(
+                    new ItemStack(OreDictionary.getOres(ModNames.DUST_SILKY).get(0).getItem(), 4),
+                    new ItemStack(OreDictionary.getOres(ModNames.DUST_PEGAGIN).get(0).getItem()),
+                    Blocks.wool);
+
+
+
+            GameRegistry.addShapelessRecipe(
+                    new ItemStack(OreDictionary.getOres(ModNames.DUST_SILKY).get(0).getItem(), 8),
+                    new ItemStack(OreDictionary.getOres(ModNames.DUST_SPECTRA).get(0).getItem()),
+                    Blocks.wool);
+
+
+        GameRegistry.addShapelessRecipe(
+                new ItemStack(OreDictionary.getOres(ModNames.DUST_SILKY_GUNPOWDER).get(0).getItem()),
+                new ItemStack(OreDictionary.getOres(ModNames.DUST_SILKY).get(0).getItem()),
+                Items.gunpowder);
+
         GameRegistry.addRecipe(new ItemStack(OreDictionary.getOres(ModNames.BLOCK_DECOR).get(0).getItem(), 32), new Object[]{
                 "SDS",
                 "DSD",
@@ -79,11 +127,22 @@ public class EquineMagic
                 'D', new ItemStack(OreDictionary.getOres(ModNames.DUST_CHROMA).get(0).getItem()),
                 'S', Blocks.stone
         });
-	}
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event)
-	{
-        // NOOP
+        GameRegistry.addRecipe(new ItemStack(OreDictionary.getOres(ModNames.BLOCK_SILKY_TNT).get(0).getItem()), new Object[]{
+                "SDS",
+                "DSD",
+                "SDS",
+                'D', Blocks.sand,
+                'S', new ItemStack(OreDictionary.getOres(ModNames.DUST_SILKY_GUNPOWDER).get(0).getItem())
+        });
+
+        GameRegistry.addRecipe(new ItemStack(OreDictionary.getOres(ModNames.BLOCK_SOLAR_CAULDRON).get(0).getItem()), new Object[]{
+                "IGI",
+                "GCG",
+                "IGI",
+                'G', Blocks.glass_pane,
+                'I', Items.iron_ingot,
+                'C', Items.cauldron
+        });
     }
 }
