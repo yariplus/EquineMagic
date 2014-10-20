4f861b48fb23c15d513355f819f4b2db88315dae
Timothy Fike <tafike@gmail.com>
2014-10-20 15:59:51 -0400

    Textures changes

---
+++ src/main/java/com/yaricraft/equinemagic/items/ItemCrystalPrimatic.java
@@ -0,0 +1,17 @@
+package com.yaricraft.equinemagic.items;
+
+import com.yaricraft.equinemagic.creativetab.CreativeTabEquineMagic;
+import com.yaricraft.equinemagic.reference.ModNames;
+
+/**
+ * Created by Yari on 9/20/2014.
+ */
+public class ItemCrystalPrimatic extends EquineMagicItem
+{
+    public ItemCrystalPrimatic()
+    {
+        super();
+        this.setUnlocalizedName(ModNames.ITEM_CRYSTAL_PRIMATIC);
+        this.setCreativeTab(CreativeTabEquineMagic.tabEquineMagic);
+    }
+}
